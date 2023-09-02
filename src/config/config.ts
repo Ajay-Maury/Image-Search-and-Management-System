ef2ee002e2da05aac7d6c5e7ad0e30bed75a9c4b
Ajay Maurya <ajaymaurya6798@gmail.com>
2023-09-02 14:06:40 +0530

    Image management Complete

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,6 +1,5 @@
-const CONFIG = {
-    IMAGE_SEARCH_SERVICE_URL: 'http://localhost:3000'
-    // IMAGE_SEARCH_SERVICE_URL: 'https://frail-ruby-penguin.cyclic.app'
+const CONFIG = { 
+    IMAGE_SEARCH_SERVICE_URL: 'https://frail-ruby-penguin.cyclic.app'
   };
   
   export const config = CONFIG;
