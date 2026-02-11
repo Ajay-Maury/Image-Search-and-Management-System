17756b42140e00e4b29363e6397b1c2d4126e088
Ajay-Maury <ajaymaurya6798@gmail.com>
2026-02-11 12:25:51 +0530

    updated config

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,6 +1,11 @@
-const CONFIG = { 
-    IMAGE_SEARCH_SERVICE_URL: 'https://frail-ruby-penguin.cyclic.app'
-  };
-  
-  export const config = CONFIG;
-  
+// const CONFIG = {
+//   IMAGE_SEARCH_SERVICE_URL: process.env.IMAGE_SEARCH_SERVICE_URL,
+// };
+
+// export const config = CONFIG;
+
+const CONFIG = {
+  IMAGE_SEARCH_SERVICE_URL: import.meta.env.VITE_IMAGE_SEARCH_SERVICE_URL,
+};
+
+export const config = CONFIG;
